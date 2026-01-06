907c42acd55a73a4f5ba8eba785e56ffad6e5785
SaharTec <sahary153@gmail.com>
2026-01-06 18:12:15 +0200

    finishd item.h and item.cpp

--- final_project/Item.h
+++ final_project/Item.h
@@ -14,9 +14,9 @@ private:
 public:
     Item(int id, const string& name);
     int getId() const;              //get the id code of the item
-    string getName() const;        //get the name of the item
+    string& getName() ;             //get the name of the item
     bool isAvailable() const;       //get if the item is available
-    string& getBorrower() const;    //return the name of the borrowed name(if the item is't avalibale)
+    string& getBorrower() ;         //return the name of the borrowed name(if the item is't avalibale)
 
     void borrow(const string& username);
     /**

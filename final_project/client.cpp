1434e29756ab5f1dc16d1ecc7eb5275954142d6a
SaharTec <sahary153@gmail.com>
2026-01-06 18:23:07 +0200

    .

--- final_project/client.cpp
+++ final_project/client.cpp
@@ -6,14 +6,3 @@
 #include <sstream>
 #include <thread>
 using namespace std;
-
-int nain(){
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    sockaddr_in srv{};
-    srv.sin_family = AF_INET;
-    srv.sin_port = htons(5555);
-    inet_pton(AF_INET, "127.0.0.1", &srv.sin_addr);
-
-    connect(fd, (sockaddr*)&srv, sizeof(srv)); //conect to the server
-}
